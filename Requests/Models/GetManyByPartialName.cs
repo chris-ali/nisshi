d83f03d8c0ee35722c293294b5403e2be4cc901c
Christopher Ali <christopher.ali@zenetex.com>
2021-09-29 13:26:18 -0400

    - Added controllers for Nisshi entities

--- Requests/Models/GetManyByPartialName.cs
+++ Requests/Models/GetManyByPartialName.cs
@@ -9,9 +9,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Nisshi.Requests.Models 
 {
-    public class GetManyByPartialCode
+    public class GetManyByPartialName
     {
-        public record Query(string partialCode) : IRequest<IList<Model>>;
+        public record Query(string partialName) : IRequest<IList<Model>>;
 
         public class QueryHandler : BaseRequest, IRequestHandler<Query, IList<Model>>
         {
@@ -21,11 +21,11 @@ namespace Nisshi.Requests.Models
 
             public async Task<IList<Model>> Handle(Query request, CancellationToken cancellationToken)
             {
-                if (string.IsNullOrEmpty(request.partialCode) || request.partialCode.Length < 3)
+                if (string.IsNullOrEmpty(request.partialName) || request.partialName.Length < 3)
                     return null;
                 
                 var data = await context.Models
-                    .Where(x => x.ModelName.StartsWith(request.partialCode))
+                    .Where(x => x.ModelName.StartsWith(request.partialName))
                     .ToListAsync(cancellationToken);
                 
                 return data;
