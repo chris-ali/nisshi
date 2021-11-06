5a987bb63ea3038be74adeca6be2c4e7610fcba5
Christopher Ali <christopher.ali@protonmail.com>
2021-11-06 09:21:48 -0400

    Initial Setup - Removed unused layout and auth components (chat, messages, shortcuts, search, unlock) - Changed auth component styles to 'classic'

--- src/Nisshi/ClientApp/src/app/layout/layouts/vertical/classy/classy.module.ts
+++ src/Nisshi/ClientApp/src/app/layout/layouts/vertical/classy/classy.module.ts
@@ -8,11 +8,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { FuseNavigationModule } from '@fuse/components/navigation';
 import { FuseFullscreenModule } from '@fuse/components/fullscreen/fullscreen.module';
 import { LanguagesModule } from 'app/layout/common/languages/languages.module';
-import { MessagesModule } from 'app/layout/common/messages/messages.module';
 import { NotificationsModule } from 'app/layout/common/notifications/notifications.module';
-import { QuickChatModule } from 'app/layout/common/quick-chat/quick-chat.module';
-import { SearchModule } from 'app/layout/common/search/search.module';
-import { ShortcutsModule } from 'app/layout/common/shortcuts/shortcuts.module';
 import { UserModule } from 'app/layout/common/user/user.module';
 import { SharedModule } from 'app/shared/shared.module';
 import { ClassyLayoutComponent } from 'app/layout/layouts/vertical/classy/classy.component';
@@ -31,11 +27,7 @@ import { ClassyLayoutComponent } from 'app/layout/layouts/vertical/classy/classy
         FuseFullscreenModule,
         FuseNavigationModule,
         LanguagesModule,
-        MessagesModule,
         NotificationsModule,
-        QuickChatModule,
-        SearchModule,
-        ShortcutsModule,
         UserModule,
         SharedModule
     ],
