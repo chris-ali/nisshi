a35723fae637b226709bdb103fa68914da2099f1
Christopher Ali <christopher.ali@protonmail.com>
2021-11-06 10:42:46 -0400

    Added initial transloco translations for German and Japanese

--- src/Nisshi/ClientApp/src/app/core/transloco/transloco.module.ts
+++ src/Nisshi/ClientApp/src/app/core/transloco/transloco.module.ts
@@ -18,8 +18,12 @@ import { TranslocoHttpLoader } from 'app/core/transloco/transloco.http-loader';
                         label: 'English'
                     },
                     {
-                        id   : 'tr',
-                        label: 'Turkish'
+                        id   : 'de',
+                        label: 'German'
+                    },
+                    {
+                        id   : 'jp',
+                        label: 'Japanese'
                     }
                 ],
                 defaultLang         : 'en',
